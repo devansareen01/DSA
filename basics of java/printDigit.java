4c8adfd22a764b54747c561acea760bcc5d3099e
devansareen01 <97303760+devansareen01@users.noreply.github.com>
2022-04-23 23:21:46 +0530

    Update printDigit.java

--- basics of java/printDigit.java
+++ basics of java/printDigit.java
@@ -5,7 +5,7 @@ public class printDigit {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
-        String string_n = Integer.toString(n);
+        String string_n = Integer.toString(n);//converting integer to string
         for (int i = 0; i < string_n.length(); i++) {
             System.out.println(string_n.charAt(i));
         }
