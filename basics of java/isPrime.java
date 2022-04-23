3d1ec1553fc7e9e6abfe9f95ddc4eb3c10323136
devansareen01 <97303760+devansareen01@users.noreply.github.com>
2022-04-23 16:26:15 +0530

    Add files via upload
    check wheather number is prime or not

---
+++ basics of java/isPrime.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class isPrime {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int t = scn.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = scn.nextInt();
+
+            int count = 0;
+
+            for (int j = 2; j * j <= n; j++) {
+                if (n % j == 0) {
+                    count++;
+                }
+
+            }
+            if (count == 0) {
+                System.out.println("prime");
+            } else {
+                System.out.println("not prime");
+            }
+        }
+    }
+}
