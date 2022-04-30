23632194e7908dda463d66a6f93efb394dc7aa2f
Devan sareen <97303760+devansareen01@users.noreply.github.com>
2022-04-30 06:09:06 +0530

    subtraction between max and min elememnt

---
+++ Array/spanOFArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class spanOFArray {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+
+        }
+        int spanOfArray = max - min;
+        System.out.println(spanOfArray);
+    }
+}
