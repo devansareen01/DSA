18f753b74d84e744cc3da24ded1416dbcc95a127
Devan sareen <97303760+devansareen01@users.noreply.github.com>
2022-05-03 23:40:51 +0530

    Create subArrays.java
    printing all sub arrays

---
+++ Array/subArrays.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int arr[] = new int [n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i; j < arr.length; j++) {
+        for (int k = i; k <= j; k++) {
+          System.out.print(arr[k] + "\t");
+        }
+        System.out.println();
+
+      }
+    }
+
+  }
+
+}
