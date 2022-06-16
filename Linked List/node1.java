a6b26c5bfca3e44464e8a8e221c0163bee82d386
devansareen01 <devansareen6@gmail.com>
2022-06-16 23:22:48 +0530

    node1

--- Linked List/node1.java
+++ Linked List/node1.java
@@ -1,9 +1,10 @@
-public class node<t> {
+public class node1<t> {
     t data; // data is our node's data at head position
-    node<t> next; // next is referencing to next postion in node
+    node1<t> next; // next is referencing to next postion in node
 
-    node(t data) {
+    node1(t data) {
         this.data = data;
         next = null;
     }
+
 }
