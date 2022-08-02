1247bac97818bae66911e8c98bf6565f81a94143
Devan sareen <97303760+devansareen01@users.noreply.github.com>
2022-08-03 00:10:05 +0530

    Update NodeWithMaximumSubTreeSum.java

--- Trees/NodeWithMaximumSubTreeSum.java
+++ Trees/NodeWithMaximumSubTreeSum.java
@@ -1,18 +1,25 @@
-static int ms = Integer.MIN_VALUE;  //global variables 
+public class nodeWithMaximumSubTreeSum {
     static int msn = 0;
-    public static int ReturnSumCalcMaxsumSubtree(TreeNode<Integer> root)  //by travel and change strategy we are returning sum of every subtree and in the way we will get opportunity to find maximum subtree sum also for that we are maintaining the global variables to store the maximum values 
-    {
-       int sum = 0;
-       for(int i = 0;i<root.children.size();i++)
-       {
-           int csum = ReturnSumCalcMaxsumSubtree(root.children.get(i));
-           sum += csum; //adding in the postorder first left child will be added then right then outside the loop root will be added in the sum 
-       }
-       sum+=root.data;
-       if(sum > ms)
-       {
-         ms = sum;
-         msn = root.data;
-       }
-       return sum;
-    } 
+    static int ms = Integer.MIN_VALUE;
+
+    public static int returnMaxSubTreeSum(TreeNode<Integer> root) {
+        int sum = 0;
+        for (int i = 0; i < root.children.size(); i++) {
+            int cms = returnMaxSubTreeSum(root.children.get(i));
+            sum += cms;
+        }
+        sum += root.data;
+        if (sum > ms) {
+            ms = sum;
+            msn = root.data;
+        }
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode<Integer> root = TreeUse.takeInput();
+        returnMaxSubTreeSum(root);// travel and change stratergy// calculating some thing else
+        System.out.println(msn);// and returning something else
+    }
+}
