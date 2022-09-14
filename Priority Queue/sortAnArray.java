6787396aad0bb2a8f3d71a455cefcb90c4e411df
Devan sareen <97303760+devansareen01@users.noreply.github.com>
2022-09-14 16:23:05 +0530

    sorting an array using priority queue

---
+++ Priority Queue/sortAnArray.java
@@ -0,0 +1,15 @@
+class Solution 
+{ 
+    int[] sortArr(int[] arr, int n) 
+    { 
+        int ans[] = new int[n];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i =0;i<n ;i++){
+            pq.add(arr[i]);
+        }
+        for(int i =0;i<n;i++){
+            ans[i]=pq.poll();
+        }
+        return ans;
+    }
+} 
