d71e27065121adb4e83835ec757dc8c5ed13a919
Devan Sareen <devansareen6@gmail.com>
2022-10-03 11:52:42 +0530

    count climb stairs paths

---
+++ Dynamic Programming/climbStairs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class climbStairs {
+ 
+
+    public static int countp(int n) {
+        int dp[] = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            if (i == 1) {
+                dp[i] = dp[i - 1];
+            } else if (i == 2) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int cp = countp(n);
+        System.out.println(cp);
+    }
+}
