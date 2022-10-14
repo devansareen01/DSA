ef0cb62528586093ee500beffe31768e38aa7d1c
Devan Sareen <devansareen6@gmail.com>
2022-10-14 12:41:41 +0530

    binary srting count without consecutive zeros

---
+++ Dynamic Programming/countOfBinaryStrings.java
@@ -0,0 +1,20 @@
+public class countOfBinaryStrings {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int oldzeros = 1;// 
+        int oldones = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int nzeros = oldones;
+            int nones = oldones + oldzeros;
+
+            oldones = nones;
+            oldzeros = nzeros;
+        }
+        System.out.println(oldones + oldzeros);
+        
+    }
+}
